b94445636bfe9abc7fa135dd6e4436974ce1568f
burn_sours <burn@lara-croft.tv>
2026-02-01 20:21:27 +0000

    framework: tomb123 patch files

---
+++ src/framework/games/tomb123/patches/patch4.1/tomb123.exe.js
@@ -0,0 +1,25 @@
+/** tomb123.exe - Patch 4.1 */
+module.exports = {
+    variables: {
+        ExitingGame: { Address: "0x2f35ec", Type: "Int8" },
+        Level: { Address: "0x263CD0", Type: "Int32" },
+        GameVersion: { Address: "0xe4bd8", Type: "Int32" },
+        LaraAppearanceModern: { Address: "0x263CD8", Type: "Block", Size: "0xd" },
+        LaraAppearanceModernOutfit: { Address: "0x263CD8", Type: "Int32" },
+        LaraAppearanceModernSunglasses: { Address: "0x263CE4", Type: "Int8" },
+        IsPhotoMode: { Address: "0x263D04", Type: "Int32" },
+        IsPhotoModeUI: { Address: "0x263D08", Type: "Int32" },
+        GameSettings: { Address: "0x2641C4", Type: "UInt8" },
+        ResolutionH: { Address: "0x2f35e0", Type: "Int32" },
+        ResolutionH2: { Address: "0x2f35f8", Type: "Int32" },
+        DevMode: { Address: "0x264190", Type: "Int8" },
+        DevModeSpeed: { Address: "0x2641a0", Type: "Int32" },
+        MoreSettings: { Address: "0x2641C6", Type: "Int8" },
+    },
+
+    hooks: {
+        KeyboardInput: { Address: "0x1910", Params: ["uint", "int"], Return: "void" },
+        TickFunction: { Address: "0x7dc0", Params: ["pointer"], Return: "void" },
+        UpdateTickRef: { Address: "0x7ea0", Params: [], Return: "void" },
+    }
+};
