b9b8157e74141c9126d17b4271f2172c8da43370
BurnAI <burn+ai@lara-croft.tv>
2026-02-04 01:00:14 +0000

    fix: Clone return type void -> pointer in patches and doc

--- src/framework/games/tomb123/patches/patch4.1/tomb2.dll.js
+++ src/framework/games/tomb123/patches/patch4.1/tomb2.dll.js
@@ -64,7 +64,7 @@ module.exports = {
         SoundEffect: { Address: "0x987d0", Params: ['int', 'pointer', 'int'], Return: 'int' },
         RenderUI: { Address: "0x34df0", Params: [], Return: 'void' },
         Menu: { Address: "0x37010", Params: ['int'], Return: 'int' },
-        Clone: { Address: "0xd8fa0", Params: ['pointer', 'pointer', 'uint64'], Return: 'void' },
+        Clone: { Address: "0xd8fa0", Params: ['pointer', 'pointer', 'uint64'], Return: 'pointer' },
         AddText: { Address: "0xae090", Params: ['int', 'int', 'int', 'pointer'], Return: 'pointer' },
         DrawSetup: { Address: "0x80830", Params: ['int', 'pointer'], Return: 'void' },
         DrawRect: { Address: "0x816c0", Params: ['int', 'int', 'int', 'int', 'uint64', 'uint64'], Return: 'void' },
