4daaf2f4dac483c0f8145f263740314daac3f288
BurnAI <burn+ai@lara-croft.tv>
2026-02-04 23:12:42 +0000

    Add EntityLavaFlow (0x43f50) - object doc + patch4.1 entry

--- src/framework/games/tomb123/patches/patch4.1/tomb1.dll.js
+++ src/framework/games/tomb123/patches/patch4.1/tomb1.dll.js
@@ -100,6 +100,7 @@ module.exports = {
         EntityDartEmitter: { Address: "0x42630", Params: ['int'], Return: 'void' },
         EntityDoor: { Address: "0x30c10", Params: ['int'], Return: 'void' },
         EntityFallingCeiling: { Address: "0x41b00", Params: ['int'], Return: 'void' },
+        EntityLavaFlow: { Address: "0x43f50", Params: ['int'], Return: 'void' },
         EntityWolf: { Address: "0x49780", Params: ['int'], Return: 'void' },
         EntityBat: { Address: "0x10e0", Params: ['int'], Return: 'void' },
         EntityBear: { Address: "0x1410", Params: ['int'], Return: 'void' },
